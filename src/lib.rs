3badd38a891add92f3dbb9bf10bd5e3849a4d9cd
Ji Chen <jaykchen@gmail.com>
2024-03-26 20:49:53 -0400

    clean template to populate

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
-use chrono::{Datelike, Duration, NaiveDate, NaiveDateTime, NaiveTime, Timelike, Utc};
+pub mod db_updater;
+use db_updater::*;
 use dotenv::dotenv;
 use flowsnet_platform_sdk::logger;
-use http_req::{
-    request::{Method, Request},
-    response::Response,
-    uri::Uri,
-};
 use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+use std::collections::HashMap;
 use std::env;
-use webhook_flows::{handler, request_handler, send_response};
+use webhook_flows::{
+    create_endpoint, request_handler,
+    route::{get, post, route, RouteError, Router},
+    send_response,
+};
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
@@ -16,7 +18,7 @@ pub async fn on_deploy() {
     create_endpoint().await;
 }
 
-#[request_handler]
+#[request_handler(get, post)]
 async fn handler(
     _headers: Vec<(String, String)>,
     _subpath: String,
@@ -30,20 +32,20 @@ async fn handler(
         .insert("/projects/:project_name", vec![get(projects_list)])
         .unwrap();
     router
-        .insert("/issues/:project_name", vec![get(issues_list)])
+        .insert("/issues/:issue_id", vec![post(approve_issue_budget)])
         .unwrap();
 
-    router
-        .insert("/project/:project_name", vec![post(decline_project)])
-        .unwrap();
+    // router
+    //     .insert("/project/:project_name", vec![post(decline_project)])
+    //     .unwrap();
 
-    router
-        .insert("/project/:project_name", vec![post(approve_budget)])
-        .unwrap();
+    // router
+    //     .insert("/project/:project_name", vec![post(approve_budget)])
+    //     .unwrap();
 
-    router
-        .insert("/project/:project_name", vec![post(final_approval)])
-        .unwrap();
+    // router
+    //     .insert("/project/:project_name", vec![post(final_approval)])
+    //     .unwrap();
 
     if let Err(e) = route(router).await {
         match e {
@@ -57,60 +59,60 @@ async fn handler(
     }
 }
 
-async fn track_and_redirect(
+async fn approve_issue_budget(
     _headers: Vec<(String, String)>,
     _qry: HashMap<String, Value>,
     _body: Vec<u8>,
 ) {
-    let urls_map = create_map().await;
-
-    match _qry.get("file_name") {
+    let issue_id = match _qry.get("issue_id") {
         Some(m) => match serde_json::from_value::<String>(m.clone()) {
-            Ok(key) => match urls_map.contains_key(&key) {
-                true => {
-                    let download_url = match urls_map.get(&key) {
-                        Some(u) => u,
-                        None => {
-                            log::error!("missing download_url for file: {}", key);
-                            return;
-                        }
-                    };
-                    let download_count = match store_flows::get(&key) {
-                        Some(val) => match serde_json::from_value::<i32>(val) {
-                            Ok(n) => n + 1,
-                            Err(_e) => {
-                                log::error!("failed to parse download_count from store: {}", _e);
-                                1
-                            }
-                        },
-                        None => 1,
-                    };
-                    store_flows::set(&key, serde_json::json!(download_count), None);
-
-                    log::info!("{} downloaed {} times", key, download_count);
-
-                    send_response(
-                        302, // HTTP status code for Found (Redirection)
-                        vec![
-                            ("Location".to_string(), download_url.to_string()), // Redirect URL in the Location header
-                        ],
-                        Vec::new(), // No body for a redirection response
-                    );
-                }
-
-                false => {
-                    log::error!("invalid file_name: {}", key);
-                    return;
-                }
-            },
+            Ok(key) => key,
             Err(_e) => {
-                log::error!("failed to parse file_name: {}", _e);
+                log::error!("failed to parse issue_id: {}", _e);
                 return;
             }
         },
         _ => {
-            log::error!("missing file_name");
+            log::error!("missing issue_id");
             return;
         }
-    }
+    };
+
+    let issue_budget = match _qry.get("issue_budget") {
+        Some(m) => match serde_json::from_value::<String>(m.clone()) {
+            Ok(key) => key.parse::<i32>().unwrap_or_default(),
+            Err(_e) => {
+                log::error!("failed to parse issue_budget: {}", _e);
+                return;
+            }
+        },
+        _ => {
+            log::error!("missing issue_budget");
+            return;
+        }
+    };
+
+    let pool = get_pool().await;
+
+    let _ = approve_issue_budget_in_db(&pool, &issue_id, issue_budget).await;
+}
+
+async fn projects_list(
+    _headers: Vec<(String, String)>,
+    _qry: HashMap<String, Value>,
+    _body: Vec<u8>,
+) {
+    // match _qry.get("file_name") {
+    //     Some(m) => match serde_json::from_value::<String>(m.clone()) {
+    //         Ok(key) => "file_name".to_string(),
+    //         Err(_e) => {
+    //             log::error!("failed to parse file_name: {}", _e);
+    //             return;
+    //         }
+    //     },
+    //     _ => {
+    //         log::error!("missing file_name");
+    //         return;
+    //     }
+    // }
 }
